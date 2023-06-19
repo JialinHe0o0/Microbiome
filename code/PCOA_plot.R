6e2f01c12c7f83592ff940a68cc7b3acc9f73adb
JIALIN HE <125544531+JialinHe0o0@users.noreply.github.com>
2023-06-19 15:39:57 +0800

    Update PCOA_plot.R

--- code/PCOA_plot.R
+++ code/PCOA_plot.R
@@ -30,11 +30,11 @@ PCOA_plot <- function(microdat,metadata,group,
   }else{
     # calculate P value
     set.seed(seed)
-    adonis_microdat <- adonis2(microdat~group_in_function,
-                               data = metadata,
-                               distance = 'bray', permutations = 999)
-    p.val <- round(adonis_microdat[1,5],3)
-    variance <- round(adonis_microdat[1,3]*100,2)
+    Permanova <- adonis2(microdat~group_in_function,
+                         data = metadata,
+                         distance = 'bray', permutations = 999)
+    p.val <- round(Permanova[1,5],3)
+    variance <- round(Permanova[1,3]*100,2)
     variance <- paste0(variance,'%')
     label <- bquote(atop(R^2 == .(variance),  italic(P)==.(p.val)))
     
@@ -81,75 +81,40 @@ PCOA_plot <- function(microdat,metadata,group,
     ymin <- min(ymin)
     ymax <- max(ymax)
   }
-  if(len==2){
-    plot <- ggplot(site)+
-      geom_point(aes(x=pcoa1, y=pcoa2,
+  
+  plot <- ggplot(site)+
+    geom_point(aes(x=pcoa1, y=pcoa2,
+                   color = group_in_function),
+               size = 0.8)+
+    stat_ellipse(aes(x = pcoa1, y = pcoa2, 
                      color = group_in_function),
-                 size = 0.8)+
-      stat_ellipse(aes(x = pcoa1, y = pcoa2, 
-                       color = group_in_function),
-                   level = 0.95, linetype = 2,linewidth = 0.8,
-                   show.legend = FALSE) +
-      geom_point(data = group_average, 
-                 aes(x = pcoa1, y = pcoa2, 
-                     color = group_in_function),
-                 size = 2.2, show.legend = FALSE)+
-      scale_x_continuous(limits = c(xmin-(xmax-xmin)*0.025,
-                                    xmax+(xmax-xmin)*0.025))+
-      scale_y_continuous(limits = c(ymin-(ymax-ymin)*0.025,
-                                    ymax+(ymax-ymin)*0.025))+
-      guides(color=guide_legend(override.aes = list(size=2.5)))+
-      labs(title = title,x = pcoa1,y = pcoa2,color = '')+
-      scale_color_manual(values = color)+
-      annotate('text', label = label, 
-               x = xmax*0.9, y = ymax*0.95, 
-               size = 3.6, family = 'serif')+
-      theme_bw()+
-      theme(legend.key = element_blank(),
-            plot.title = element_text(family = "serif",size=12),
-            axis.title.x= element_text(family = "serif",size=12),
-            axis.title.y= element_text(family = "serif",size=12),
-            axis.text.x= element_text(family = "serif",size=12),
-            axis.text.y= element_text(family = "serif",size=12),
-            legend.text = element_text(family = "serif",size=10),
-            axis.ticks.length.x = unit(0.1,'cm'),
-            axis.ticks.length.y = unit(0.1,'cm'),
-            plot.margin = unit(c(0.2,0.2,0.2,0.2),"cm"))
-  }else{
-    plot <- ggplot(site)+
-      geom_point(aes(x=pcoa1, y=pcoa2,
-                     color = group_in_function),
-                 size = 0.8)+
-      stat_ellipse(aes(x = pcoa1, y = pcoa2, 
-                       color = group_in_function),
-                   level = 0.95, linetype = 2,linewidth = 0.8,
-                   show.legend = FALSE) +
-      geom_point(data = group_average, 
-                 aes(x = pcoa1, y = pcoa2, 
-                     color = group_in_function),
-                 size = 2.2, show.legend = FALSE)+
-      scale_x_continuous(limits = c(xmin-(xmax-xmin)*0.025,
-                                    xmax+(xmax-xmin)*0.025))+
-      scale_y_continuous(limits = c(ymin-(ymax-ymin)*0.025,
-                                    ymax+(ymax-ymin)*0.025))+
-      guides(color=guide_legend(override.aes = list(size=2.5)))+
-      labs(title = title,x = pcoa1,y = pcoa2,color = '')+
-      scale_color_manual(values = color)+
-      annotate('text', label = label, 
-               x = xmax*0.9, y = ymax*0.95, 
-               size = 3.6, family = 'serif')+
-      theme_bw()+
-      theme(legend.key = element_blank(),
-            plot.title = element_text(family = "serif",size=12),
-            axis.title.x= element_text(family = "serif",size=12),
-            axis.title.y= element_text(family = "serif",size=12),
-            axis.text.x= element_text(family = "serif",size=12),
-            axis.text.y= element_text(family = "serif",size=12),
-            legend.text = element_text(family = "serif",size=10),
-            axis.ticks.length.x = unit(0.1,'cm'),
-            axis.ticks.length.y = unit(0.1,'cm'),
-            plot.margin = unit(c(0.2,0.2,0.2,0.2),"cm"))
-  }
+                 level = 0.95, linetype = 2,linewidth = 0.8,
+                 show.legend = FALSE) +
+    geom_point(data = group_average, 
+               aes(x = pcoa1, y = pcoa2, 
+                   color = group_in_function),
+               size = 2.2, show.legend = FALSE)+
+    scale_x_continuous(limits = c(xmin-(xmax-xmin)*0.025,
+                                  xmax+(xmax-xmin)*0.025))+
+    scale_y_continuous(limits = c(ymin-(ymax-ymin)*0.025,
+                                  ymax+(ymax-ymin)*0.025))+
+    guides(color=guide_legend(override.aes = list(size=2.5)))+
+    labs(title = title,x = pcoa1,y = pcoa2,color = '')+
+    scale_color_manual(values = color)+
+    annotate('text', label = label, 
+             x = xmax*0.9, y = ymax*0.95, 
+             size = 3.6, family = 'serif')+
+    theme_bw()+
+    theme(legend.key = element_blank(),
+          plot.title = element_text(family = "serif",size=15),
+          axis.title.x= element_text(family = "serif",size=15),
+          axis.title.y= element_text(family = "serif",size=15),
+          axis.text.x= element_text(family = "serif",size=12),
+          axis.text.y= element_text(family = "serif",size=12),
+          legend.text = element_text(family = "serif",size=10),
+          axis.ticks.length.x = unit(0.1,'cm'),
+          axis.ticks.length.y = unit(0.1,'cm'),
+          plot.margin = unit(c(0.2,0.2,0.2,0.2),"cm"))
   
   if(!is.null(path)){
     ggsave(plot = plot,
