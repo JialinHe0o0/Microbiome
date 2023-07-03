17783f424be556674d932476e2e41274fc05e56a
JIALIN HE <125544531+JialinHe0o0@users.noreply.github.com>
2023-07-03 11:51:45 +0800

    Create detect_rate.R

---
+++ Microbiome_Filter/detect_rate.R
@@ -0,0 +1,21 @@
+# Jialin He 202307
+
+detect_rate <- function(dat,
+                        sample_in_row = T,
+                        proportion = 0.05){
+  if(sample_in_row == F){
+    dat <- t(dat) %>% as.data.frame()
+  }
+  
+  a <- function(x){
+    detect <- sum(x>0)/length(x)
+    return(detect)
+  }
+  rate <- sapply(dat,a)
+  dat <- dat[,rate>=proportion]
+  
+  list <- list(rate=rate,
+               dat=dat)
+  return(list)
+}
+
